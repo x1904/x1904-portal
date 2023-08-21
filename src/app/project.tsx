fb364a9230b0fe8e8f86c5ee032896db32fb52e1
x1904 <dev@1904.app>
2023-08-21 02:16:52 +0200

    add props icon in project + add icon mesh app

--- src/app/project.tsx
+++ src/app/project.tsx
@@ -1,21 +1,31 @@
 import { Box } from '@mui/material';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export interface projectProps {
     title: string;
     description: string;
     link: string;
+    logo: string;
 }
 
-const Project: React.FC<projectProps> = ({title, description, link}) => {
+const Project: React.FC<projectProps> = ({title, description, link, logo}) => {
     const containerStyle = {
         margin: "2%",
         width: "100%"
     }
     return (
-        <Link href={link} className='h-full rounded-3xl p-10 w-full bg-white bolt text-gray-800'>
-            <span className='flex text-4xl md:text-7xl lg:text-7xl underline hover:decoration-blue-600 relative'>{title}</span>
-            <span className='block text-xl m-2'>{description}</span>
+        <Link href={link} className='h-full text-center rounded-3xl p-10 my-12 w-full md:w-3/4 xl:w-1/2 bg-gray-200 bolt text-gray-800'>
+            { logo != "" && <Image
+                className='block m-auto lg:float-right'
+                alt="project logo"
+                src={logo}
+                width={150}
+                height={150}
+                />}
+            <span className='block text-4xl md:text-7xl lg:text-7xl xl:text-9xl underline hover:decoration-blue-600 relative'>{title}</span>
+            <span className='block text-xl lg:text-2xl xl:text-3xl m-2'>{description}</span>
+            
         </Link>
     )
 }
